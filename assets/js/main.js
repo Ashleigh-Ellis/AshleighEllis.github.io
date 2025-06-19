fa7fc041743b5c02c67e74164a6e8b1c69b7dcd8
Ashleigh Ellis <ashleighellis1999@gmail.com>
2025-06-19 21:56:40 +0200

    Update main.js
    Remove Poptrox lightbox and image hover overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,19 +98,7 @@
 		// Lightbox gallery.
 			$window.on('load', function() {
 
-				$('#two').poptrox({
-					caption: null,
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a.image',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-				});
+				
 
 			});
 
